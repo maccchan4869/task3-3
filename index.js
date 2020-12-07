1da4e5c65c88a8bd77a36f54c82937cf4aef8ae5
maccchan4869 <ymatsuo554@gmail.com>
2020-12-07 09:12:18 +0900

    課題3-4　実装後

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ window.onload = () => {
   const tasks = [];
   const btnAddTask = document.getElementById('btnAddTask');
   const tbody = document.getElementById('tbodyTasks');
+  const btnCheckStatus = document.getElementsByName('checkedStatus');
   const taskStatus = {
-    working: '0',   // 作業中
-    complete: '1'   // 完了
+    all: '0',       // すべて
+    working: '1',   // 作業中
+    complete: '2'   // 完了
   };
-  
-  // 追加ボタン押下時の処理
-  btnAddTask.addEventListener('click', () => {
-    const inputTask = document.getElementById('textInputTask');
-    tasks.push({
-      content: inputTask.value,
-      status: taskStatus.working
+
+  const setEvent= () => {
+    // 追加ボタン押下時の処理
+    btnAddTask.addEventListener('click', () => {
+      const inputTask = document.getElementById('textInputTask');
+      tasks.push({
+        content: inputTask.value,
+        status: taskStatus.working
+      });
+      dispTask();
+      inputTask.value = '';
     });
-    dispTask();
-    inputTask.value = '';
-  });
+
+    // ラジオボタン変更時の処理をセット
+    btnCheckStatus.forEach((e) => {
+      e.addEventListener('change', () => {
+        changeTask();
+      });
+    });
+  }
 
   /**
    * タスクを表示
@@ -28,11 +39,26 @@ window.onload = () => {
       const row = document.createElement('tr');
       const statusVal = value.status === taskStatus.working ? '作業中' : '完了';
       // 行追加
-      row.appendChild(createCell(index));
-      row.appendChild(createCell(value.content));
+      row.appendChild(createCell(index, true, value.status));
+      row.appendChild(createCell(value.content, false));
       row.appendChild(createButton(statusVal, index, changeStatus));
       row.appendChild(createButton('削除', index, deleteTask));
       tbody.appendChild(row);
+    });
+    changeTask();
+  };
+
+  /**
+   * 表示するタスクを変更
+   */
+  const changeTask = () => {
+    const rows = tbody.querySelectorAll('tr');
+    let checkedVal = taskStatus.all;
+    btnCheckStatus.forEach((e) => {
+      if(e.checked) checkedVal = e.value;
+    });
+    rows.forEach((e) => {
+      e.style.display = e.cells[0].dataset.status === checkedVal || checkedVal === taskStatus.all ? '' : 'none';
     });
   };
 
@@ -57,12 +83,15 @@ window.onload = () => {
   /**
    * Cellを生成
    *  
-   * @param {string} value  - cellの文字
-   * @return {object} cell  - cellのDOM
+   * @param {string}  value     - cellの文字
+   * @param {boolean} isDataSet - データセットフラグ
+   * @param {string}  status    - タスクのステータス
+   * @return {object} cell      - cellのDOM
    */
-  const createCell = (value) => {
+  const createCell = (value, isDataSet, status) => {
     const cell = document.createElement('td');
     cell.textContent = value;
+    if(isDataSet) cell.dataset.status = status;
     return cell;
   };
 
@@ -83,4 +112,6 @@ window.onload = () => {
     cell.appendChild(button);
     return cell;
   };
+
+  setEvent();
 };
